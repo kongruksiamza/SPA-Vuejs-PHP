c84ec5a88684d7d78f8096ed418560e368d81deb
kongruksiamza <kongruksiamza123@gmail.com>
2020-06-02 14:32:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-</head>
+<\head>
 <body>
         <div class="container" id="app">
                 <h2 align="center">{{message}}</h2>
